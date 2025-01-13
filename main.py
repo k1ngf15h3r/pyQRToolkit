0386fa6f952406fc00c102776a6878ea26d2b8d2
Benjamin Hagels <hagels@pcconsult.de>
2025-01-13 10:51:21 +0100

    gui to method for vcard qrcode

--- main.py
+++ main.py
@@ -16,51 +16,75 @@ class MainWindow(QWidget):
         self.setLayout(main_layout)
 
         # create a tab widget
-        tabs = QTabWidget(
+        self.tabs = QTabWidget(
             self, 
             movable=True, 
             tabShape=QTabWidget.TabShape.Rounded,
             tabPosition=QTabWidget.TabPosition.North
         )
 
+        self.vornameLine = QLineEdit(self)
+        self.nachnameLine = QLineEdit(self)
+        self.strasseLine = QLineEdit(self)
+        self.plzLine = QLineEdit(self)
+        self.ortLine = QLineEdit(self)
+        self.handyLine = QLineEdit(self)
+        self.emailLine = QLineEdit(self)
+        self.websiteLine = QLineEdit(self)
+
+        generateBtn = QPushButton('Generate')
+        generateBtn.clicked.connect(self.get_tab_index)
+        cancelBtn = QPushButton('Cancel')
 
         # personal page
         vcard_page = QWidget(self)
         layout = QFormLayout()
         vcard_page.setLayout(layout)
-        layout.addRow('Vorname:', QLineEdit(self))
-        layout.addRow('Nachname:', QLineEdit(self))
-        layout.addRow('Strasse:', QLineEdit(self))
-        layout.addRow('PLZ:', QLineEdit(self))
-        layout.addRow('Ort:', QLineEdit(self))
-        layout.addRow('Handy:', QLineEdit(self))
-        layout.addRow('E-Mail:', QLineEdit(self))
-        layout.addRow('Website:', QLineEdit(self))
+        layout.addRow('Vorname:', self.vornameLine)
+        layout.addRow('Nachname:', self.nachnameLine)
+        layout.addRow('Strasse', self.strasseLine)
+        layout.addRow('PLZ:', self.plzLine)
+        layout.addRow('Ort:', self.ortLine)
+        layout.addRow('Handy:', self.handyLine)
+        layout.addRow('E-Mail:', self.emailLine)
+        layout.addRow('Website:', self.websiteLine)
 
         # contact page
         contact_page = QWidget(self)
         layout = QFormLayout()
         contact_page.setLayout(layout)
-        layout.addRow('Phone Number:', QLineEdit(self))
-        layout.addRow('Email Address:', QLineEdit(self))
+        #layout.addRow('Handy:', self.handyLine)
+        #layout.addRow('E-Mail:', self.emailLine)
 
         # add page to the tab widget
-        tabs.addTab(vcard_page, 'VCARD')
-        tabs.addTab(contact_page, 'Contact Info')
+        self.tabs.addTab(vcard_page, 'VCARD')
+        self.tabs.addTab(contact_page, 'Contact Info')
 
-        main_layout.addWidget(tabs, 0, 0, 2, 1)
-        main_layout.addWidget(QPushButton('Save'), 2, 0, alignment=Qt.AlignmentFlag.AlignLeft)
-        main_layout.addWidget(QPushButton('Cancel'), 2, 0, alignment=Qt.AlignmentFlag.AlignRight)
+        main_layout.addWidget(self.tabs, 0, 0, 2, 1)
+        main_layout.addWidget(generateBtn, 2, 0, alignment=Qt.AlignmentFlag.AlignLeft)
+        main_layout.addWidget(cancelBtn, 2, 0, alignment=Qt.AlignmentFlag.AlignRight)
 
         self.show()
 
-    def make_vcard_qr_code():
-        name = input("Enter your name: ")
-        email = input("Enter your email: ")
-        phone = input("Enter your mobile phone number: ")
-        street = input("Enter your street: ")
-        city = input("Enter your city: ")
-        code = input("Enter your code: ")
+    def get_tab_index(self):
+        tabIndex = self.tabs.currentIndex()
+        if(tabIndex == 0):
+            self.make_vcard_qr_code(
+                name = str(self.vornameLine.text() + " " + self.nachnameLine.text()),
+                email = self.emailLine.text(),
+                phone = self.handyLine.text(),
+                street = self.strasseLine.text(),
+                city = self.ortLine.text(),
+                code = self.plzLine.text(),
+            )
+
+    def make_vcard_qr_code(self, **kwargs):
+        name = kwargs.get('name')
+        email = kwargs.get('email')
+        phone = kwargs.get('phone')
+        street = kwargs.get('street')
+        city = kwargs.get('city')
+        code = kwargs.get('code')
 
         # Create a vCard
         vcard = vobject.vCard()
